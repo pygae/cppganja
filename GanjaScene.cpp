76e16e7bcc1b85ad543b2d9471fe1bfd425ab7ab
hugo hadfield <hadfield.hugo@gmail.com>
2019-07-23 10:57:15 +0100

    Added some comments

--- GanjaScene.cpp
+++ GanjaScene.cpp
@@ -130,19 +130,20 @@ public:
 
 int main(){
 
+    // A couple of example CGA objects
     std::vector<double> test_obj{0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,4.23135483,4.23135483,0.48031496,0.48031496,0.95680003,1.2228812,1.2228812,-0.06350011,-0.28372776,0.,0.,0.,0.,0.,0.};
     std::vector<double> test_obj2{0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,-0.08525168,3.49464756,3.52703718,-1.59496133,-1.62835966,0.76309611,0.64874369,0.65012918,0.18968262,-0.22823195,0.,0.,0.,0.,0.,0.};
 
+    // Make the scene and add the objects
     GanjaScene gs = GanjaScene();
     gs.add_object(test_obj);
     gs.add_object(test_obj2, Color::RED);
 
-    std::cout << gs.as_string() + "\n";
-
+    // State what signature our algebra is from 
     std::vector<double> sig{1.0,1.0,1.0,1.0,-1.0};
-
     std::string html = generate_full_html(gs.as_string(), sig, 0.1);
 
+    // Write the output to an html file
     std::ofstream myfile;
     myfile.open ("example.html");
     myfile << html;
